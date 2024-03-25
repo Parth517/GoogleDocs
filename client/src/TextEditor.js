56e6834d760c0392fa7f664166236650bb60e96a
Parth Deshpande <parthjun27@gmail.com>
2024-03-26 01:37:29 +0530

    Contecting backend and frontend

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css"
-import { useCallback } from "react";
+import { useCallback,useEffect } from "react";
+import io from 'socket.io-client'
 export default function TextEditor(){
+    const [socket,setSocket]=useState();
+    const [quill,setQuill]=useState();
+    useEffect(()=>{
+        const s=io("http://localhost:3001")
+        setSocket(s)
+        return ()=>{
+            s.disconnect();
+        }
+    },[])
 
     const ToolBar_Options=[
         [{header:[1,2,3,4,5,6, false]}],
@@ -10,7 +20,7 @@ export default function TextEditor(){
         [{list:"ordered"},{list:"bullet"}],
         ["bold","italic", "underline"],
         [{color:[]},{background:[]}],
-        [{script:"sub"},{script:"super"}]
+        [{script:"sub"},{script:"super"}],
         [{align: []}],
         ["image","blockquote","code-block"],
         ["clean"]
@@ -20,8 +30,18 @@ export default function TextEditor(){
         wrapper.innerHTML="";
         const editor=document.createElement('div')
         wrapper.append(editor)
-        new Quill(editor,{ theme:"snow",modules: {toolbar:ToolBar_Options} })
+        const q=new Quill(editor,{ theme:"snow",modules: {toolbar:ToolBar_Options} })
+        setQuill(q);
     },[])
+
+    useEffect(()=>{
+        quill.on('text-change',(delta,oldDelta,source)=>{
+            if(source!=='user'){
+                
+            }
+        })
+    })
+
     return  <div className="container" ref={wrapperRef}></div>
     
 }
