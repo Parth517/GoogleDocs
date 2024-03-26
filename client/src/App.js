89e0cedfddad1a14b2a9870c5253499dc69f1a39
Parth Deshpande <parthjun27@gmail.com>
2024-03-26 12:31:20 +0530

    Multiple Documents

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+
+import {v4 as uuidV4} from 'uuid';
 
 function App() {
   return (
-    <TextEditor />
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Navigate to={`/documents/${uuidV4()}`}/>}/>
+        <Route
+        path="/documents/:id"
+        element={<TextEditor />}
+      />
+      </Routes>
+    </Router>
   )
 }
 
